2c6bf0bd381afa21924772b4250af615f4bc8e43
Eben Vranken <36372346+EbenVranken@users.noreply.github.com>
2021-03-24 19:48:58 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var isPaused = false,
+    onBreak = false;
+$('.pause').on('click', function(e) {
+    e.preventDefault();
+    isPaused = true;
+});
+
+$('.play').on('click', function(e) {
+    e.preventDefault();
+    isPaused = false;
+});
+
+function startTimer(workTime, pauseTime, display) {
+    var timer = workTime,
+        minutes, seconds;
+    var breakCount = 0;
+    setInterval(function() {
+        if (!isPaused) {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            document.querySelector(".timer").innerHTML = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                if (!onBreak) {
+                    timer = pauseTime
+                    breakCount++;
+                    onBreak = true;
+                    if (breakCount >= 4) {
+                        timer = workTime;
+                        breakCount = 0;
+                    }
+                } else {
+                    timer = workTime;
+                    onBreak = false;
+                }
+            }
+        }
+    }, 1000);
+}
+
+window.onload = function() {
+    var workTime = 60 * 25;
+    var pauseTime = 60 * 5;
+
+    breakCount = 0;
+    display = document.querySelector('#time');
+    startTimer(workTime, pauseTime, display);
+};
